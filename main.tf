9dbec3c4dfa714019d45783920304635027bed49
NeelakanthS <nilakanthsonnad@gmail.com>
2024-09-13 20:53:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
 resource "aws_cloudwatch_dashboard" "my_cloudwatch_dashboard" {
 dashboard_name = "${var.app_group}-${lower(var.app_env)}-${var.app_name}-dashboard"
- dashboard_body = templatefile("${path.module}/dashboard-cloudwatch.json", {
-    log-group = "cloudtrail-log-group" # Log group of the cloudtrail or log group name where the ECR history/API Calls is stored
-  })
+ dashboard_body = templatefile("${path.module}/dashboard-cloudwatch.json", 
+  )
 }
