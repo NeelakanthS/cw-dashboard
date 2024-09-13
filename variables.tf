59e6b20fe7c9753624012f8c954740313e3c723a
NeelakanthS <nilakanthsonnad@gmail.com>
2024-09-13 20:47:45 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "app_group" {
+  description = "Application Organisation group name"
+  type        = string
+  default     = "mnv"
+}
+
+variable "app_env" {
+  description = "AWS app environment"
+  type        = string
+}
+
+variable "app_name" {
+  description = "Application Name"
+  type        = string
+}
