e6f9cc56e0ea2e3be1d5204e9760ab91b15ef86f
NeelakanthS <nilakanthsonnad@gmail.com>
2024-09-13 20:56:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_cloudwatch_dashboard" "my_cloudwatch_dashboard" {
 dashboard_name = "${var.app_group}-${lower(var.app_env)}-${var.app_name}-dashboard"
- dashboard_body = templatefile("${path.module}/dashboard-cloudwatch.json", 
+ dashboard_body = templatefile("${path.module}/cw-dashboard.json", 
   )
 }
